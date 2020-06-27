f1793182f9f338c3816c2e7fd561084d5d79c5ac
unknown <ohndaniel@gmail.com>
2020-06-27 14:44:16 -0700

    Implement React Hook to toggle between themes.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  function handleClick() {
+    let newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+
+    localStorage.setItem("theme", newTheme);
+    document.documentElement.setAttribute("data-user-color-scheme", newTheme);
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-user-color-scheme", theme);
+  }, [theme]);
+
   return (
     <div className="App">
       <h1>Heading</h1>
       <p>This is the text that goes here!</p>
-      <button>Dark Mode</button>
+      <button onClick={handleClick}>Dark Mode</button>
     </div>
   );
 }
